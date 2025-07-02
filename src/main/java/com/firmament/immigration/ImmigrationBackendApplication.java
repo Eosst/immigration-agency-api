13f863a45571876b1efd149ac3d6628b0b6d8b79
Eosst <medmimouni98@gmail.com>
2025-07-02 14:34:13 +0100

    Initial commit: Spring Boot project setup with dependencies

---
+++ src/main/java/com/firmament/immigration/ImmigrationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.firmament.immigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firmament/immigration/ImmigrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firmament.immigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmigrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
